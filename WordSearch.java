6b53a044b307b0729a1335b01423667425fd8eb9
Trent1048 <trentbultsma@gmail.com>
2020-01-24 14:29:23 -0800

    add getRandomChar()

--- WordSearch.java
+++ WordSearch.java
@@ -2,15 +2,19 @@ import java.util.ArrayList;
 
 public class WordSearch {
 
-    private static char[][] unsolvedSearch;
-    private static char[][] solvedSearch;
+    private static char[][] unsolvedBoard;
+    private static char[][] solvedBoard;
     private static ArrayList<String> words; // words in the word search
 
     public static void main(String[] args) {
-        unsolvedSearch = new char[][] {{'a','b','c'},{'d','e','f'},{'g','h','i'}};
-        display(unsolvedSearch);
+        unsolvedBoard = new char[][] {{'a','b','c'},{'d','e','f'},{'g','h','i'}};
+        display(unsolvedBoard);
+        for(int i = 0; i < 20; i++){
+            System.out.print(getRandomChar());
+        }
     }
 
+    // displays a 2d char array, either the solved or unsolved boards
     private static void display(char[][] wordSearch) {
         for (int row = 0; row < wordSearch.length; row++) {
             for (int col = 0; col < wordSearch[0].length; col++) {
@@ -18,5 +22,9 @@ public class WordSearch {
             }
             System.out.println();
         }
+    }
+
+    private static char getRandomChar() {
+        return (char)(97 + (int)(Math.random() * 26));
     }
 }
