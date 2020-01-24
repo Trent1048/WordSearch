05384b58155f54a847fe74966997ac48f0cfc44a
Trent1048 <trentbultsma@gmail.com>
2020-01-24 14:43:40 -0800

    add intro()

--- WordSearch.java
+++ WordSearch.java
@@ -1,12 +1,11 @@
-import java.util.ArrayList;
-
 public class WordSearch {
 
     private static char[][] unsolvedBoard;
     private static char[][] solvedBoard;
-    private static ArrayList<String> words; // words in the word search
+    private static String words; // words in the word search split up by spaces
 
     public static void main(String[] args) {
+        intro();
         unsolvedBoard = new char[][] {{'a','b','c'},{'d','e','f'},{'g','h','i'}};
         display(unsolvedBoard);
         for(int i = 0; i < 20; i++){
@@ -18,10 +17,20 @@ public class WordSearch {
     private static void display(char[][] wordSearch) {
         for (int row = 0; row < wordSearch.length; row++) {
             for (int col = 0; col < wordSearch[0].length; col++) {
-                System.out.print(wordSearch[row][col] + " ");
+                System.out.print(wordSearch[row][col] + "   ");
             }
             System.out.println();
         }
+    }
+
+    private static void intro() {
+        System.out.println("\nWelcome to my word search program!\n" +
+                "It will allow you to generate a word search\n" +
+                "Please select an option:\n" +
+                "\tGenerate a new board (g)\n" +
+                "\tPrint out the word search (p)\n" +
+                "\tDisplay the solution (s)\n" +
+                "\tQuit the program (q)\n");
     }
 
     private static char getRandomChar() {
